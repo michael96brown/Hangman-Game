049a82f1f8be62e5e134477893c094f0a7af0ca3
michael brown <michaelbrown@michaels-MacBook-Pro.local>
2017-11-11 09:24:56 -0700

    unfinished files

---
+++ assets/javascript/game.js
@@ -0,0 +1,23 @@
+window.onload = function () {
+
+	var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+        'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+        't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  
+	var categories;
+
+	var chosenCategory;
+
+	var word;
+
+	var guess;
+
+	var geusses = [ ]; 
+
+	var lives;
+	
+	var counter;
+
+	var showLives = document.getElementById("mylives");
+
+	
